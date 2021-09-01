27e699e6a23e747d0d768a0aeae027c072f30ce9
shabnam <shabnam@gamil.com>
2021-09-01 02:40:04 -0700

    sshipping & finantial

---
+++ financial/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from.models import Cart, Payments, Off
+admin.site.register(Off)
+admin.site.register(Cart)
+admin.site.register(Payments)
---
+++ shipping/models.py
@@ -0,0 +1,33 @@
+import datetime
+
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+from financial.models import Orders
+
+
+class Post(models.Model):
+    type_of_shipping = [
+        ('post', 'post'),
+        ('pishtaz', 'pishtaz'),
+        ('aadi', 'aadi'),
+        ('ersal_forooshgahi', "ersal_forooshgahi"),
+        ('ersal_pekmotori', 'ersal_peykmotori '),
+
+    ]
+
+    date_chices = [
+        (datetime.date)
+    ]
+
+    day_division = [
+        ('صبح', '۸ تا ۱۲ بعد از ظهر'),
+        ('ظهر', '۱۲ تا ۶ عصر'),
+        ("شب", "۶ عصر تا ۱۲ شب"),
+    ]
+
+    post = models.CharField(max_length=20, choices=type_of_shipping)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    time_recived = models.CharField(max_length=2, choices=day_division)
+    date_recived = models.DateField()
