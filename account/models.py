82ce8c71df77345fb2944b35aa1fd7f20e256541
shabnam <shabnam@gamil.com>
2021-09-08 14:58:30 +0430

    register

--- account/models.py
+++ account/models.py
@@ -5,18 +5,22 @@ from utils import upload_image_path
 
 
 class Addresses(models.Model):
-     province = models.CharField(max_length=50)
+    province = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    full_address = models.CharField(max_length=200)
+
 
 class CommonUsersField(User):
     image = models.ImageField(upload_to=upload_image_path, verbose_name="تصویر کاربر", null=True, blank=True)
     address = models.ForeignKey(Addresses, on_delete=models.CASCADE)
+    phone = models.PositiveIntegerField(null=True)
 
     class Meta:
         abstract = True
 
 
 class Suppliers(CommonUsersField):
-    pass
+    store_name = models.CharField(max_length=200)
 
 
 class Customers(CommonUsersField):
