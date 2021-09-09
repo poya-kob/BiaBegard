3cb2bae3d4b6bdca145c76b762936593a4cece97
poya shadman <poya_kob@live.com>
2021-09-09 09:08:26 +0430

    home page

--- BiaBegard/urls.py
+++ BiaBegard/urls.py
@@ -18,9 +18,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import home_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_page),
     path('products/', include("content.urls")),
     path('ckeditor/', include("ckeditor_uploader.urls"))
 
