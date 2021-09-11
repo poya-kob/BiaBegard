f5d7edbddf21f6dbf0d2fac2437257b5b733efd0
poya shadman <poya_kob@live.com>
2021-09-11 17:22:56 +0430

    image filde adde to brand model,product gallary adde to detail view,gallery save limited to 3,show product price changed,url for changed products deatil view

--- utils.py
+++ utils.py
@@ -21,3 +21,5 @@ def upload_image_path(instance, filename):
         return f"product/{date.year}/{date.month}/{date.day}/{new_name}"
     elif type(instance).__name__ == "ProductsGalleries":
         return f"Products_Galleries/{date.year}/{date.month}/{date.day}/{new_name}"
+    elif type(instance).__name__ == "Brands":
+        return f"Brands/{date.year}/{date.month}/{date.day}/{new_name}"
