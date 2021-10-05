d7496fefe4a5c6618c38a6f2325c0b07599e79e2
shabnam <shabnam@gamil.com>
2021-10-05 11:39:23 +0330

    sth

--- comment/models.py
+++ comment/models.py
@@ -6,7 +6,6 @@ from content.models import Products
 
 
 class Comments(models.Model):
-
     title = models.CharField(max_length=150, verbose_name='عنوان نظر')
     description = models.TextField(verbose_name="متن اصلی نظر")
 
@@ -15,7 +14,8 @@ class Comments(models.Model):
                               related_name='comment_reply')
     created_time = jmodels.jDateTimeField(auto_now_add=True, verbose_name='زمان ایجاد نظر')
     active = models.BooleanField(default=False, verbose_name='فعال / غیر فعال')
-    product = models.ForeignKey(Products,on_delete=models.CASCADE, verbose_name='محصولات', related_name='comments', null=True, blank=True)
+    product = models.ForeignKey(Products, on_delete=models.CASCADE, verbose_name='محصولات', related_name='comments',
+                                null=True, blank=True)
 
     def __str__(self):
         return self.title
