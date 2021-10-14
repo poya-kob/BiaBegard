ab977eed134b6c5fb10c55485dcbc81856b000bd
shabnam <shabnam@gamil.com>
2021-10-14 18:33:13 +0330

    setting

--- BiaBegard/settings.py
+++ BiaBegard/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 MY_DOMAIN = '127.0.0.1:8000'
 # Application definition
 
@@ -160,10 +160,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [(
-        BASE_DIR / 'static/')
-]
-# STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn", "static_root")
+# STATICFILES_DIRS = [
+#     (BASE_DIR / 'static/'),
+# ]
+STATIC_ROOT = BASE_DIR / 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "static_cdn", "media_root")
